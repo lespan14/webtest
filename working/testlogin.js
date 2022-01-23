53f43fd18d222a572c80cda803ed3f76a261100c
lespan14 <pandaallen@naver.com>
2022-01-23 21:06:37 +0800

    script

--- working/testlogin.js
+++ working/testlogin.js
@@ -3,16 +3,20 @@ let container = document.querySelector('div#container');
 let id = container.querySelector('input#loginid');
 let pw = container.querySelector('input#loginpw');
 
-let cookie = document.cookie;
-document.cookie = "user=A";
+let namec = container.querySelector('input#makecokname');
+let value = container.querySelector('input#makecokval');
+let exp = container.querySelector('input#makecokexp');
+
+let delname = container.querySelector('input#delcokname');
 
 const hlg = document.getElementById('loginfunc');
 hlg.addEventListener('click',function() {
     
+
     if(id.value == "root" && pw.value == "root"){
         alert("Hello!");
         console.log("Hello",id.value);
-        setCookie();
+        setCookie(namec,value,exp);
         //login cookie save 
     }else{
         alert("No");
@@ -22,8 +26,32 @@ hlg.addEventListener('click',function() {
 
 })
 
-var setCookie = function() {
-	var date = new Date();
-	date.setTime(date.getTime() + 24*60*60*1000);
-	document.cookie = 'A' + '=' + '1' + ';expires=' + date.toUTCString() + ';path=/';
-};
+let cokdelbtn = container.querySelector('button#cokdelbtn');
+cokdelbtn.onclick = function(){
+
+    deleteCookie(delname);
+    alert('Del cookie');
+
+}
+
+let cokmakebtn = container.querySelector('button#cokmakebtn');
+cokmakebtn.onclick = function() {
+
+    setCookie(namec,value,exp);
+}
+
+let setCookie = function(name, value, exp) {
+	let date = new Date();
+	date.setTime(date.getTime() + exp*24*60*60*1000);
+	document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
+
+let deleteCookie = function(name) {
+	document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+}
+
+let getCookieval = function(name) {
+	let value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    
+	return value? value[2] : null;
+}
